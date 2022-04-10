046a43ffa8f3a27416f50c9e37a3b791c92b6196
Spencer Watari <deanwatari@gmail.com>
2022-04-10 10:57:50 -0400

    eddit readme

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,7 @@ function getResults(){
   let choice3=0;
 
   let choicesArray=document.getElementsByTagName('input');
+  console.log(choicesArray);
 
   for(let i=0; i<choicesArray.length;i++) {
     if(choicesArray[i].checked) {
@@ -20,7 +21,13 @@ function getResults(){
       }
     }
   }
+  console.log(choice1);
+  console.log(choice2);
+  console.log(choice3);
+
   let totalScore=Math.max(choice1,choice2,choice3);
+  console.log(totalScore);
+
   let surveyResponse;
   if(choice1==totalScore) {
     surveyResponse="Ruby";
@@ -32,4 +39,7 @@ function getResults(){
     surveyResponse="C#";
   }
   alert("Your Programing Language is " + surveyResponse);
+}
+function resetForm() {
+  location.reload;
 }
