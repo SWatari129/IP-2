36010594ce4bd2f551f30fe3720e3291f23b3996
Spencer Watari <deanwatari@gmail.com>
2022-04-10 10:30:59 -0400

    edit JS

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+function getResults(){
+  let choice1=0;
+  let choice2=0;
+  let choice3=0;
+
+  let choicesArray=document.getElementsByTagName('input');
+
+  for(let i=0; i<choicesArray.length;i++) {
+    if(choicesArray[i].checked) {
+      if(choicesArray[i] == 'c1') {
+        choice1=choice1+1;
+      }
+      if(choicesArray[i] == 'c2') {
+        choice2=choice2+1;
+      }
+      if(choicesArray[i] == 'c3') {
+        choice3=choice3+1;
+      }
+    }
+  }
+  let totalScore=Math.max(choice1,choice2,choice3);
+  let surveyResponse;
+  if(choice1==totalScore) {
+    surveyResponse="Ruby";
+  }
+  if(choice2==totalScore) {
+    surveyResponse="Javescript";
+  }
+  if(choice3==totalScore) {
+    surveyResponse="C#";
+  }
+  alert("Your Programing Language is " + surveyResponse);
+}
