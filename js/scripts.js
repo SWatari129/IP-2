120283d3af027417d7127aea0f837b474f70a195
Spencer Watari <deanwatari@gmail.com>
2022-04-17 11:36:51 -0400

    edit html for style

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,26 @@
-
-
 function getResults(){
   let choice1=0;
   let choice2=0;
   let choice3=0;
 
   let choicesArray=document.getElementsByTagName('input');
-  console.log(choicesArray);
 
   for(let i=0; i<choicesArray.length;i++) {
     if(choicesArray[i].checked) {
-      if(choicesArray[i] == 'c1') {
+      if(choicesArray[i].value === 'c1') {
         choice1=choice1+1;
       }
-      if(choicesArray[i] == 'c2') {
+      if(choicesArray[i].value === 'c2') {
         choice2=choice2+1;
       }
-      if(choicesArray[i] == 'c3') {
+      if(choicesArray[i].value === 'c3') {
         choice3=choice3+1;
       }
-    }
+    }  
   }
-  console.log(choice1);
-  console.log(choice2);
-  console.log(choice3);
+
 
   let totalScore=Math.max(choice1,choice2,choice3);
-  console.log(totalScore);
 
   let surveyResponse;
   if(choice1==totalScore) {
